4a67e3e3bb1c69acb5529a792c255c4b86adf30e
Rayvvin <rayvvin01@gmail.com>
2025-05-12 09:25:06 +0100

    update

--- medusa-config.js
+++ medusa-config.js
@@ -58,15 +58,15 @@ const plugins = [
       },
     },
   },
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-      automatic_payment_methods: true,
-      capture: true
-    },
-  },
+  // {
+  //   resolve: `medusa-payment-stripe`,
+  //   options: {
+  //     api_key: process.env.STRIPE_API_KEY,
+  //     webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+  //     automatic_payment_methods: true,
+  //     capture: true
+  //   },
+  // },
   {
     resolve: `medusa-plugin-smtp`,
     options: {
